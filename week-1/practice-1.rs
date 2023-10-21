7b3b5b2b2b1948c484bdd94197c50a810d1864df
o.hillaryCSC101 <chibuike.onyeka@gmail.com>
2023-10-21 17:58:15 +0100

    (my first commit)

---
+++ week-1/practice-1.rs
@@ -0,0 +1,10 @@
+fn main(){
+	printIn!("welcome to COS 101!")
+	printIn!("1. Distinguish between computer and computer programming,/n
+		2. Develop some techniques in computer,/n
+		3. Understand the different areaas of study in computer science,/n
+		4. Conversant with applications of computer science,/n
+		5. Navigate the career prospects in computer science,/n
+		6. Conversant with computer programming concepts.")
+
+}
